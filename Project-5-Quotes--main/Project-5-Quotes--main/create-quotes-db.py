5e38c8a418e75b34ca25485a1ec3274736c67065
rakeshrjsr <88228780+rakeshrjsr@users.noreply.github.com>
2024-05-09 22:56:16 -0500

    Add files via upload

---
+++ Project-5-Quotes--main/Project-5-Quotes--main/create-quotes-db.py
@@ -0,0 +1,28 @@
+# Create a Mongita database with movie information
+import json
+from mongita import MongitaClientDisk
+
+# Modify the quotes_data to include comments
+quotes_data = [
+    {"text": "I'm hungry. When's lunch?", "author": "jyo", "owner": "jyo", "comments": []},
+    {"text": "You threw that ball. You go get it.", "author": "jyo", "owner": "jyo", "comments": []},
+]
+
+
+# create a mongita client connection
+client = MongitaClientDisk()
+
+# create a movie database
+quotes_db = client.quotes_db
+
+# create a quotes collection
+quotes_collection = quotes_db.quotes_collection
+
+# empty the collection
+quotes_collection.delete_many({})
+
+# put the quotes in the database
+quotes_collection.insert_many(quotes_data)
+
+# make sure the quotes are there
+print(quotes_collection.count_documents({}))
